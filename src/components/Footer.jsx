21a0688c072a0fc63d3fa7aeadfb405c939f81ed
Rigved-Harmalkar <89383329+Rigved-Harmalkar@users.noreply.github.com>
2023-06-04 18:19:44 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div className='footer'>
+        <h1>Creative</h1>
+        <p>@all rights reserved.</p>
+    </div>
+    <div>
+        <h5>Follow Us</h5>
+        <a href="https://www.linkedin.com/in/rigved-harmalkar-8392b7201/" target={"blank"}>LinkedIn</a>
+        <a href="https://www.instagram.com/rigvedhhhh/" target={"blank"}>Instagram</a>
+        <a href="https://github.com/rigved-harmalkar" target={"blank"}>GitHub</a>
+
+
+
+    </div>
+
+  </footer>
+}
+
+export default Footer
